83996d49b7f5955f9fb6ed4b355727a45c4da37f
George <gosha.gosha9696@gmail.com>
2022-03-25 20:50:47 +0300

    added axes labels

--- src/pages/YouTubeBiggestChannels.tsx
+++ src/pages/YouTubeBiggestChannels.tsx
@@ -4,6 +4,7 @@ import {useCSV} from '@utils/hooks/useCSV';
 
 const width = 960;
 const height = 500;
+const margin = {top: 20, right: 20, bottom: 20, left: 250};
 
 interface YouTubeCSV {
 	'Subscribers (millions)': string;
@@ -28,27 +29,56 @@ export const YouTubeBiggestChannels: React.FC = () => {
 	const yValue = (d: any) => d.Channel;
 	const xValue = (d: any) => d.Subs;
 
+	const innerHeight = height - margin.top - margin.bottom;
+	const innerWidth = width - margin.right - margin.left;
+
 	const yScale = scaleBand()
 		.domain(topTen.map(yValue))
-		.range([0, height]);
+		.range([0, innerHeight])
+		.paddingInner(0.15);
 
 	const xScale = scaleLinear()
 		.domain([0, max(topTen, xValue)])
-		.range([0, width]);
+		.range([0, innerWidth]);
 
-	console.log(xScale.ticks());
+	const checkYScaleTick = (tick: string) => {
+		const result = yScale(tick);
+		return result ? result : 0;
+	};
 
 	return (
 		<>
 			YouTube Channels
 			<svg width={width} height={height}>
-				{topTen.map((d: YouTubeCSV) => <rect
-					x={0}
-					y={yScale(d.Channel)}
-					width={xScale(d.Subs)}
-					height={yScale.bandwidth()}
-					key={d.Channel}
-				/>)}
+				<g transform={`translate(${margin.left},${margin.top})`}>
+					{xScale.ticks().map((tick: number) => (
+						<g key={tick} transform={`translate(${xScale(tick)},0)`}>
+							<line y2={innerHeight} stroke={'black'} />
+							<text
+								dy={'0.71em'}
+								y={innerHeight} style={{textAnchor: 'middle'}}
+							>
+								{tick}
+							</text>
+						</g>
+					))}
+					{yScale.domain().map((tick: string) => (
+						<g key={tick} transform={`translate(0,${checkYScaleTick(tick) + (yScale.bandwidth() / 2)})`}>
+							<text
+								dy={'0.32em'}
+								style={{textAnchor: 'end'}}>
+								{tick}
+							</text>
+						</g>
+					))}
+					{topTen.map((d: YouTubeCSV) => <rect
+						x={0}
+						y={yScale(d.Channel)}
+						width={xScale(d.Subs)}
+						height={yScale.bandwidth()}
+						key={d.Channel}
+					/>)}
+				</g>
 			</svg>
 		</>
 	);
