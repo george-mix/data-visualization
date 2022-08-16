615290a1a51e752637e41b1fc653f800d9189913
George <gosha.gosha9696@gmail.com>
2022-08-16 19:29:45 +0300

    feat: add translation provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {AppNavigation} from '@components/Navigation/AppNavigation';
 import {AppRouter} from '@utils/AppRouter';
+import {TranslationProvider} from '@utils/providers/TranslationProvider';
 
 const App: React.FC = () => (
-  <Router>
-    <AppNavigation />
-    <AppRouter />
-  </Router>
+  <Suspense fallback={<div>Loading...</div>}>
+    <TranslationProvider>
+      <Router>
+        <AppNavigation />
+        <AppRouter />
+      </Router>
+    </TranslationProvider>
+  </Suspense>
 );
 
 export default App;
---
+++ src/utils/providers/TranslationProvider.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import i18n from 'i18next';
+import {I18nextProvider, initReactI18next} from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export const TranslationProvider: React.FC = ({children}) => (
+  <I18nextProvider i18n={i18n}>
+    {children}
+  </I18nextProvider>
+);
