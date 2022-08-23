ca0200b2fd586896b32c4240a1485eb12f17c755
George <gosha.gosha9696@gmail.com>
2022-08-23 18:43:37 +0300

    feat: add store provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import {AppNavigation} from '@components/Navigation/AppNavigation';
 import {AppRouter} from '@utils/AppRouter';
 import {TranslationProvider} from '@utils/providers/TranslationProvider';
 import {StyleProvider} from '@utils/providers/StyleProvider';
+import {StoreProvider} from '@utils/providers/StoreProvider';
 
 const App: React.FC = () => (
-  <StyleProvider>
-    <Suspense fallback={<div>Loading...</div>}>
-      <TranslationProvider>
-        <Router>
-          <AppNavigation />
-          <AppRouter />
-        </Router>
-      </TranslationProvider>
-    </Suspense>
-  </StyleProvider>
+  <StoreProvider>
+    <StyleProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TranslationProvider>
+          <Router>
+            <AppNavigation />
+            <AppRouter />
+          </Router>
+        </TranslationProvider>
+      </Suspense>
+    </StyleProvider>
+  </StoreProvider>
 );
 
 export default App;
---
+++ src/utils/providers/StoreProvider.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from '../../store/store';
+
+export const StoreProvider: React.FC = ({children}) => (
+  <Provider store={store}>
+    {children}
+  </Provider>
+);
