66987819a4e02d9f37e907249bbab05b4806ef82
George <gosha.gosha9696@gmail.com>
2022-08-29 13:37:58 +0300

    fix: wrong type export path for nav list

--- src/components/Navigation/NavList.tsx
+++ src/components/Navigation/NavList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {List, ListItem, ListItemText} from '@mui/material';
 import {Link} from 'react-router-dom';
-import {AppRoute} from '@utils/commonTypes';
+import {AppRoute} from '@common/interfaces';
 import {useTranslation} from 'react-i18next';
 
 export const NavList: React.FC<{
