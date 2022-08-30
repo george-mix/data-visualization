431d0c15fa377f0888bcc53d794a01f9ef8fefe9
George <gosha.gosha9696@gmail.com>
2022-08-30 16:41:32 +0300

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
+import {mainRoutes} from '@router/routes';
+import {socialMediaList} from '@common/data';
+
+export const Footer: React.FC = () => {
+  const {t} = useTranslation();
+
+  return (
+    <footer>
+      <Container sx={{pb: 4, pt: 4}}>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography component="h2" variant="subtitle2">
+              {t('common.contacts')}
+            </Typography>
+            <List>
+              {socialMediaList.map((media) => (
+                <ListItem button key={media.name}>
+                  <a href={media.link}>
+                    <ListItemText primary={t(`${media.name}`)} />
+                  </a>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography component="h2" variant="subtitle2">
+              {t('common.pages')}
+            </Typography>
+            <List>
+              {mainRoutes.map((route) => (
+                <ListItem button key={route.name}>
+                  <Link to={route.path}>
+                    <ListItemText primary={t(`${route.name}`)} />
+                  </Link>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+        </Grid>
+        <Typography component="p" variant="body2">
+           ©2022
+        </Typography>
+      </Container>
+    </footer>
+  );
+};
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {AppNavigation} from '@components/Navigation/AppNavigation';
 import {AppRoutes} from '@router/AppRoutes';
+import {Footer} from '@components/Footer';
 
 export const AppLayout: React.FC = () => (
   <>
     <AppNavigation />
     <AppRoutes />
+    <Footer />
   </>
 );
