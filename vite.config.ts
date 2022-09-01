22f6bdd7843ee2c673917701f454518c6adde3ba
George <gosha.gosha9696@gmail.com>
2022-09-01 16:25:51 +0300

    perf: add gzip bundle compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    viteCompression(),
   ],
 });
