d849a042b45881cc47e9db0b0d5f88ca2bab85cc
George <gosha.gosha9696@gmail.com>
2022-09-14 22:01:35 +0300

    feat: create language percentage chart

---
+++ src/components/YouTubeBiggestChannels/LanguagePercentageChart/LanguagePercentageChart.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {PieArcDatum, arc, pie, scaleOrdinal} from 'd3';
+import {YouTubeChannelData} from '@helpers/common/interfaces';
+import {ResponsiveSvg} from '@components/ChartElements/ResponsiveSvg';
+import {PieChartPart} from '@components/ChartElements/PieChartPart';
+import {useColorScheme} from '@helpers/hooks/useColorScheme';
+import {useChangeLanguageInfo} from './useChangeLanguageInfo';
+import {LanguageInfo} from './LanguageInfo';
+import {FormControlLabel, Radio, RadioGroup} from '@mui/material';
+
+type ChartProps = {
+  data: YouTubeChannelData[];
+}
+
+const width = 600;
+const height = 500;
+const coefficient = width / height;
+const radius = Math.min(width, height) / 2;
+const translateWidth = radius * coefficient;
+const translateHeight = radius;
+const donutInnerRadius = radius * 0.6;
+const donutOutterRadius = radius * 0.7;
+const labelRadius = radius * 0.76;
+
+export const LanguagePercentageChart: React.FC<ChartProps> = ({data}) => {
+  const {
+    languageData,
+    getText,
+    switchQuantitySource,
+    infoType,
+  } = useChangeLanguageInfo(data);
+
+  const colorScheme = useColorScheme('schemeOne');
+  const colors = scaleOrdinal(colorScheme);
+
+  const createPie = pie<LanguageInfo>()
+    .value((d) => d.quantity)
+    .sort(null);
+
+  const createArc = arc<PieArcDatum<LanguageInfo>>()
+    .innerRadius(donutInnerRadius)
+    .outerRadius(donutOutterRadius);
+
+  const createAnnotationArc = arc<PieArcDatum<LanguageInfo>>()
+    .innerRadius(labelRadius)
+    .outerRadius(labelRadius);
+
+  const getAnnotationText = (languageArc: LanguageInfo) => {
+    const annotation = getText(languageArc);
+    return annotation;
+  };
+
+  const languageDonutArcs = createPie(languageData);
+
+  return (
+    <>
+      <ResponsiveSvg
+        width={width}
+        translateWidth={translateWidth}
+        height={height}
+        translateHeight={translateHeight}
+      >
+        {
+          languageDonutArcs.map((languageArc, index) => (
+            <PieChartPart<LanguageInfo>
+              key={languageArc.data.language}
+              arc={languageArc}
+              createArc={createArc}
+              createAnnotationArc={createAnnotationArc}
+              index={index}
+              colors={colors}
+              annotation={getAnnotationText(languageArc.data)}
+            />
+          ))
+        }
+      </ResponsiveSvg>
+      <RadioGroup row value={infoType} onChange={switchQuantitySource}>
+        <FormControlLabel
+          value="channels"
+          control={<Radio />}
+          label="Number of Channels"
+        />
+        <FormControlLabel
+          value="subscribers"
+          control={<Radio />}
+          label="Percentage of Subscribers"
+        />
+      </RadioGroup>
+    </>
+  );
+};
+
---
+++ src/components/YouTubeBiggestChannels/LanguagePercentageChart/useChangeLanguageInfo.ts
@@ -0,0 +1,80 @@
+import {useState} from 'react';
+import {flatRollup, rollups, sum} from 'd3';
+import {YouTubeChannelData} from '@helpers/common/interfaces';
+import {getPercentageString} from '@helpers/functions/mathUtils';
+import {LanguageInfo} from './LanguageInfo';
+
+type InfoType = 'subscribers' | 'channels';
+
+export const useChangeLanguageInfo = (data: YouTubeChannelData[]) => {
+  const [infoType, setinfoType] = useState<InfoType>('channels');
+
+  const switchQuantitySource = () => {
+    const newInfoType = infoType === 'subscribers' ? 'channels' : 'subscribers';
+    setinfoType(newInfoType);
+  };
+
+  const getSubscribersLanguageData = (): LanguageInfo[] => {
+    const subscribersLanguageData = flatRollup(
+      data,
+      (value) => sum(value, (channel) => channel.subscribers),
+      (channel) => channel.primaryLanguage,
+    )
+      .map((language) => ({
+        language: language[0],
+        quantity: language[1],
+      }));
+    return subscribersLanguageData;
+  };
+
+  const getChannelsLanguageData = (): LanguageInfo[] => {
+    const languageData = rollups(
+      data,
+      (v) => v.length,
+      (d) => d.primaryLanguage,
+    )
+      .map(([language, quantity]) => ({language, quantity}));
+    return languageData;
+  };
+
+  const getSubscribersText = (totalQuantity: number) => (languageData: LanguageInfo) => {
+    const percentage = getPercentageString(
+      languageData.quantity,
+      totalQuantity,
+    );
+    return `${languageData.language} (${percentage})`;
+  };
+
+  const getChannelsText = (languageData: LanguageInfo) => (
+    `${languageData.language} (${languageData.quantity})`
+  );
+
+  const getLanguageData = (infoType: InfoType) => {
+    if (infoType === 'subscribers') {
+      const languageData = getSubscribersLanguageData();
+      const totalQuantity = languageData.reduce(
+        (accumulator, language) =>
+          accumulator + language.quantity,
+        0,
+      );
+      const getText = getSubscribersText(totalQuantity);
+      return {
+        languageData: getSubscribersLanguageData(),
+        getText,
+      };
+    }
+
+    return {
+      languageData: getChannelsLanguageData(),
+      getText: getChannelsText,
+    };
+  };
+
+  const languageData = getLanguageData(infoType);
+
+  return {
+    ...languageData,
+    switchQuantitySource,
+    infoType,
+  };
+};
