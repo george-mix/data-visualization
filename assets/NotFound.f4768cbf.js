13905d8621c0ddd314188ed05a91f2dbea87d0de
George <gosha.gosha9696@gmail.com>
2022-11-21 08:39:27 +0300

    Updates

--- assets/NotFound.f4768cbf.js
+++
@@ -1,1 +0,0 @@
-import{j as r,B as n,a as o,T as a}from"./index.48276ea0.js";const e=()=>r(n,{children:[o(a,{component:"h1",variant:"h1",color:"primary.light",children:"404"}),o(a,{component:"h2",variant:"h2",color:"text.secondary",children:"Wrong page!"})]});export{e as default};
